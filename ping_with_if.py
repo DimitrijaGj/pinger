527cd63f00b5093ea96f601c73e8cbeea5e1576e
DimitirjaGj <zimzelen929@gmail.com>
2022-10-28 16:35:11 +0200

    aded text file with IP Addresses In the script list is added and time for diefrent amount of pings

--- ping_with_if.py
+++ ping_with_if.py
@@ -3,13 +3,17 @@
 import os
 
 """ Taking IP from list pings it and returns if it is down"""
-ip_list = ["1.1.1.1", "172.21.118.152", "172.21.116.58", "192.1.0.2"]
+with open("list.txt") as ip_list:
 
-for ip in ip_list:
-    response = os.popen("ping -c 4 " + ip).read()
-    if "100.0% packet loss" in response:
-        print(ip, "is unresponcive  👎  ")
+    for ip in ip_list:
+        # response = os.popen("ping -c 4 " + ip).read()
+        response = os.popen("ping -c 2 " + ip).read()
+        if "100.0% packet loss" in response:
+            print(ip, "is unresponcive  👎  ")
 
 
 #    else:
 # print(ip, "is up")
+# with 4 pings ended in 1min 50 sec
+# with 1 ping endet in 13 sec
+# with 2 pings ended in 30 sec
