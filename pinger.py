cd9e675df37bfa4675a388478ada84501c151ec8
DimitirjaGj <zimzelen929@gmail.com>
2022-10-28 15:16:45 +0200

    Ping function added

--- pinger.py
+++ pinger.py
@@ -1,8 +1,20 @@
 #!/usr/bin/env python
-''' first import the lybraries '''
 
-''' open the IP list ''
+import os
 
-''' parse trough the list '''
+# first import the lybraries
 
-''' ping the IPs '''
+""" open the IP list """
+
+""" parse trough the list """
+
+""" ping the IPs 
+"""
+print("#" * 60)
+print("-" * 60)
+ip_to_check = input("Pls enter IP...")
+print("-" * 60)
+os.system("ping -c 4 {}".format(ip_to_check))
+print("*" * 60)
+
+input("Press ENTER to exit.")
