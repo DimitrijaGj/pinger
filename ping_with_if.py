398908fc8ec306c46c75d8c0bf976e2a5e13fe63
DimitirjaGj <zimzelen929@gmail.com>
2022-10-28 15:44:10 +0200

    format the empty spaces

--- ping_with_if.py
+++ ping_with_if.py
@@ -8,7 +8,6 @@ ip_list = ["1.1.1.1", "172.21.118.152", "172.21.116.58", "192.1.0.2"]
 for ip in ip_list:
     response = os.popen("ping -c 4 " + ip).read()
     if "100.0% packet loss" in response:
-
         print(ip, "is unresponcive ")
 
 
